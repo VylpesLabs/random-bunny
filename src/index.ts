089f6934134855e65c6fd0f0487c3000fe7a996c
Ethan Lane <ethan@vylpes.com>
2023-02-22 18:18:50 +0000

    Remove maxTries variable

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ const sortable = [
     'top'
 ];
 
-export default async function randomBunny(subreddit: string, sortBy?: string, maxTries = 100): Promise<IReturnResult> {
+export default async function randomBunny(subreddit: string, sortBy?: string): Promise<IReturnResult> {
     if (!sortBy || !sortable.includes(sortBy)) sortBy = 'hot';
 
     const result = await fetch(`https://reddit.com/r/${subreddit}/${sortBy}.json`);
@@ -30,39 +30,37 @@ export default async function randomBunny(subreddit: string, sortBy?: string, ma
     }
 
     const data: IFetchResult[] = json.data.children;
-    
+
     const dataWithImages = new List<IFetchResult>(data)
         .Where(x => x!.data.url.includes('.jpg') || x!.data.url.includes('.png'))
         .ToArray();
 
-    for (let i = 0; i < maxTries; i++) {
-        const random = Math.floor((Math.random() * dataWithImages.length - 1) + 0); // Between 0 and (size - 1)
+    const random = Math.floor((Math.random() * dataWithImages.length - 1) + 0); // Between 0 and (size - 1)
 
-        const randomSelect = dataWithImages[random];
+    const randomSelect = dataWithImages[random];
 
-        if (!randomSelect) continue;
-
-        const randomData = randomSelect.data;
-
-        const redditResult: IRedditResult = {
-            Archived: randomData['archived'],
-            Downs: randomData['downs'],
-            Hidden: randomData['hidden'],
-            Permalink: randomData['permalink'],
-            Subreddit: randomData['subreddit'],
-            SubredditSubscribers: randomData['subreddit_subscribers'],
-            Title: randomData['title'],
-            Ups: randomData['ups'],
-            Url: randomData['url']
-        };
-
+    if (!randomSelect) {
         return {
-            IsSuccess: true,
-            Result: redditResult
+            IsSuccess: false,
         };
-    }
+    };
+
+    const randomData = randomSelect.data;
+
+    const redditResult: IRedditResult = {
+        Archived: randomData['archived'],
+        Downs: randomData['downs'],
+        Hidden: randomData['hidden'],
+        Permalink: randomData['permalink'],
+        Subreddit: randomData['subreddit'],
+        SubredditSubscribers: randomData['subreddit_subscribers'],
+        Title: randomData['title'],
+        Ups: randomData['ups'],
+        Url: randomData['url']
+    };
 
     return {
-        IsSuccess: false
-    }
+        IsSuccess: true,
+        Result: redditResult
+    };
 }
